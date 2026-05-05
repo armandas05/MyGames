f32936c85331e4ad658e9096d0744742c5f6a719
armandas05 <ar.bunys@gmail.com>
2026-05-05 15:36:11 +0300

    Created User and GameEntries models.

--- Data/Database/AppDbContext.cs
+++ Data/Database/AppDbContext.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyGames.Data.Database.Models;
 
 namespace MyGames.Data.Database
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<GameEntry> GameEntries { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
-
         }
 
         
