b6785a59cb7e2df05b3f14d7d181a1e1cfedeaed
armandas05 <ar.bunys@gmail.com>
2026-05-06 16:01:37 +0300

    login test in react

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,49 @@
-function LoginPage() {
-  return (
-    <div>
-      <h2>Login</h2>
-    </div>
-  );
+import { useState } from "react";
+
+function Loginpage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleLogin(event) {
+        event.preventDefault();
+
+        console.log(email);
+        console.log(password);
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <form onSubmit={handleLogin}>
+
+                <div>
+                    <label>Email</label>
+
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <label>Password</label>
+
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
+                <button type="submit">
+                    Login
+                </button>
+
+            </form>
+        </div>
+    );
 }
 
-export default LoginPage;
+export default Loginpage;
