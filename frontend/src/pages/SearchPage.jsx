fcdf73ae7da3d5ab1cb97644d7980cf682a3a778
armandas05 <ar.bunys@gmail.com>
2026-05-09 22:40:58 +0300

    minor search page tests with tailwind

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -31,35 +31,71 @@ function SearchPage() {
 
 
     return (
-        <div>
-            <h1>Search Games</h1>
+        <div className="min-h-screen bg-zinc-950 text-white p-8">
+            <h1 className="text-5xl font-bold mb-8">
+                Search Games
+            </h1>
 
             <input
                 type="text"
                 placeholder="Search game..."
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
+                className="
+                bg-zinc-900
+                border
+                border-zinc-700
+                rounded-xl
+                px-4
+                py-3
+                w-full"
             />
 
-            <button onClick={handleSearch}>
+            <button 
+                onClick={handleSearch}
+                className="
+                bg-blue-600
+                hover:bg-blue-700
+                px-6
+                rounded-xl
+                font-semibold">
                 Search
             </button>
 
-            <div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
 
                 {games.map((game) => (
-                    <div key={game.id}>
+                    <div 
+                        key={game.id}
+                        className="
+                            bg-zinc-900
+                            rounded-2x1
+                            overflow-hidden
+                            shadow-lg">
 
-                        <h3>{game.name}</h3>
 
-                        <p>{game.released}</p>
+                        <h3 className="text-xl font-bold mb-2">
+                            {game.name}
+                        </h3>
+
+                        <p className="text-zinc-400 mb-4">
+                            {game.released}
+                        </p>
 
                         <img
                             src={game.backgroundImage}
-                            width="200"
+                            className="w-full h-60 object-cover"
                         />
 
-                        <button onClick={() => addGame(game)}>
+                        <button 
+                            onClick={() => addGame(game)}
+                            className="
+                                bg-green-600
+                                hover:bg-green-700
+                                px-4
+                                py-2
+                                rounded-xl
+                                w-full">
                             Add Game
                         </button>
 
